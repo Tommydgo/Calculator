4bbdd607bce6022ee19392a782a80e456081cbc9
Tommydgo <tommy.diago@epitech.eu>
2026-06-02 09:38:16 +0000

    feat: implement calculator logic and output in main function

--- src/calculator.c
+++ src/calculator.c
@@ -1,5 +1,33 @@
 #include "global_includes.h"
 
+static int compute(unsigned long long int nb_1, char operator,  unsigned long long int nb_2, long long int total) {
+
+    switch (operator) {
+        case '+':
+            return total += nb_2;
+        case '-':
+            return total -= nb_2;
+        case '*':
+            return total *= nb_2;
+        case '/':
+            return total /= nb_2;
+        case '%':
+            return total %= nb_2;
+    }
+    return total;
+}
+
 int calculator(char **argv) {
-    return SUCCESS;
+    unsigned long long int current_nb = 0;
+    char current_operator = ' ';
+    unsigned long long int next_nb = 0;
+    long long int total = 0;
+
+    for (int i = 0; argv[i + 2] != '\0'; i + 3) {
+        current_nb = atoll(argv[i]);
+        next_nb = atoll(argv[i + 2]);
+        current_operator = argv[i];
+        total = compute(current_nb, current_operator, next_nb, total);
+    }
+    return total;
 }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include "global_includes.h"
 
 int main(int argc, char **argv) {
+    long long int total = 0;
+
     if (check_args(argc, argv) == EXIT_FAIL)
         return EXIT_FAIL;
-    return calculator(argv);
+    total = calculator(argv);
+    if (printf("%lld\n", total) < 0)
+        return EXIT_FAIL;
+    return SUCCESS;
 }
